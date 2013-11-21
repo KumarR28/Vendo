5d35ac80525f959e677833dfd7c2ad3ffa5e89ec
Kumar Rachuri <KumarR28@me.com>
2013-11-21 00:04:43 -0500

    php mail handler

--- mailer.php
+++ mailer.php
@@ -3,11 +3,12 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
  
-$to = 'youremail@domain.com';
-$subject = 'the subject';
-$message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
-$headers = 'From: youremail@domain.com' . "\r\n";
+$to = 'palai@vendo-inc.com';
+$subject = 'Message From Vendo Contact Page';
+$message = 'FROM: '.$name.' Email: '.$email.' Message: '.$message;
+$headers = 'From: mailprocessor@typeserve.com' . "\r\n";
  
+
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {  
 mail($to, $subject, $message, $headers);  
 echo "Your email was sent!";  
